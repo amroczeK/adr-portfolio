80fff7d36381da950426901e0b34109de3594501
amroczeK <mroczek9990@gmail.com>
2021-04-10 23:53:15 +0800

    nav bar grow size fix

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -9,7 +9,25 @@ const GlobalStyle = createGlobalStyle`
 }
 body {
   background: #111;
+  font-family: 'Inter', sans-serif;
 }
+h2 {
+    font-weight: lighter;
+    font-size: 1.25rem;
+  }
+h3 {
+    color: white;
+  }
+h4 {
+  font-weight: bold;
+  font-size: 2rem;
+}
+p {
+    padding: 3rem 0rem;
+    color: #ccc;
+    font-size: 1.4rem;
+    line-height: 150%;
+  }
 `;
 
 export default GlobalStyle;
--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -4,8 +4,20 @@ import styled from 'styled-components';
 
 const NavButton = ({ path, icon, title }) => {
   const { pathname } = useLocation();
+
+  const getTextWidth = (text, font) => {
+    // re-use canvas object for better performance
+    let canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement('canvas'));
+    let context = canvas.getContext('2d');
+    context.font = font;
+    let metrics = context.measureText(text);
+    return metrics.width * 0.063;
+  };
+
+  console.log(getTextWidth(title, 'lighter 1.25rem Inter'));
+
   return (
-    <Button>
+    <Button growWidth={getTextWidth(title, 'lighter 1.25rem Inter')}>
       <CircleIcon iconActive={pathname === path}>
         <h2>{title}</h2>
         <StyledIcon>{icon}</StyledIcon>
@@ -32,6 +44,7 @@ const CircleIcon = styled.div`
   border-radius: 50%;
   align-items: center;
   justify-content: center;
+  text-align: left;
   cursor: pointer;
   transition: all 0.3s ease-in-out;
 `;
@@ -51,15 +64,18 @@ const Button = styled.div`
   :hover {
     transition: all 0.3s ease-in-out;
     border-radius: 30px;
-    padding-left: 10rem;
+    //padding-left: 10rem;
+    padding-left: ${({ growWidth }) => `${growWidth + 3.25}rem;` || '3.5rem;'};
     height: 3.5rem;
     background: #ffc107;
     h2 {
+      padding-left: ${({ growWidth }) => `${growWidth}rem;` || '2rem;'};
       transition: all 0.3s ease-in-out;
-      margin-right: 10rem;
-      align-items: center;
+      //margin-right: 12rem;
+      margin-right: ${({ growWidth }) => `${growWidth + 10}rem;` || '10rem;'};
+      /* align-items: center;
       justify-content: center;
-      text-align: center;
+      text-align: center; */
       opacity: 1;
       color: white;
       line-height: 50px;
